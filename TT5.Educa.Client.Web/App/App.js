9273e10b36f00b6c95c6ec28635afc47c402ffed
aguiareduardo <eduardoaguiar@msn.com>
2016-03-29 03:38:55 +0100

    ui route

--- TT5.Educa.Client.Web/App/App.js
+++ TT5.Educa.Client.Web/App/App.js
@@ -1,54 +1,48 @@
-﻿angular.module('app', ['ngRoute'])
+﻿angular.module('app', ['ngRoute', 'ui.router'])
 
-.config(['$routeProvider',
-  function ($routeProvider) {
-      $routeProvider.
-        when('/Index.html', {
-            templateUrl: 'Views/Home/Home.html',
-            controller: 'HomeController'
-        }).
-        when('/', {
-            templateUrl: 'Views/Home/Home.html',
-            controller: 'HomeController'
-        }).
-        when('/home', {
-            templateUrl: 'Views/Home/Home.html',
-            controller: 'HomeController'
-        }).
-        when('/resultado/:query', {
-            templateUrl: 'Views/Resultado/Resultado.html',
-            controller: 'ResultadoController'
-        }).
-        when('/planos', {
-            templateUrl: 'Views/Plano/Planos.html',
-            controller: 'PlanosController'
-        }).
-        when('/plano/:id', {
-            templateUrl: 'Views/Plano/Plano.html',
-            controller: 'PlanoViewController'
-        }).
-        when('/planoEdit/:id', {
-            templateUrl: 'Views/Plano/PlanoEdit.html',
-            controller: 'PlanoEditController'
-        }).
-        when('/aulas', {
-            templateUrl: 'Views/Aula/Aulas.html',
-            controller: 'AulasController'
-        }).
-        when('/aula/:id', {
-            templateUrl: 'Views/Aula/Aula.html',
-            controller: 'AulaViewController'
-        }).
-        when('/aula/:id', {
-            templateUrl: 'Views/Aula/AulaEdit.html',
-            controller: 'AulaEditController'
-        }).
-        otherwise({
-            redirectTo: '/FileNotFound'
-        });
-  }])
-.run(['$rootScope', '$location', function ($rootScope, $location) {
-    $rootScope.$on('$routeChangeStart', function(next, current) {         
+.config(['$stateProvider', '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise("/PageNotFound");
+
+      $stateProvider
+    .state('home', {
+        url: "/home",
+        templateUrl: "Views/Home/Home.html"
+    })
+    .state('resultado', {
+        url: "/:query",
+        templateUrl: "View/Resultado/Resultado.html",
+        controller: "ResultadoController"
+    })
+    .state('aula', {
+        url: "/list",
+        templateUrl: "View/Aula/Aulas.html",
+        controller: "AulasController"
+    })
+    .state('aula.edit', {
+        url: "/:id",
+        templateUrl: "View/Aula/AulaEdit.html",
+        controller: "AulaEditController"
+    })
+    .state('aula.view', {
+        url: "/:id",
+        templateUrl: "View/Aula/AulaView.html",
+        controller: "AulaViewController"
+    })
+    .state('plano', {
+        url: "/list",
+        templateUrl: "View/Plano/Planos.html",
+        controller: "PlanosController"
+    })
+    .state('plano.edit', {
+        url: "/:id",
+        templateUrl: "View/Plano/PlanoEdit.html",
+        controller: "PlanoEditController"
+    })
+    .state('plano.view', {
+        url: "/:id",
+        templateUrl: "View/Plano/PlanoView.html",
+        controller: 'PlanoViewController'
     });
-}])
+  }]);
 
